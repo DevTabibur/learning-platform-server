08ba5db82c3bfd54d7ceb76089b7b9f733729f2d
devtabibur <tobiburrohman2@gmail.com>
2022-08-10 00:09:56 +0600

    mongodb connected and all collections is made

--- server.js
+++ server.js
@@ -13,6 +13,43 @@ app.get('/', async(req, res)=>{
     res.send("Hello world!")
 });
 
+// user:elearning
+// pswd:EWA1CgP7wfsQYb76
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://elearning:EWA1CgP7wfsQYb76@cluster0.hc4xz.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run(){
+    try{
+        await client.connect();
+        const parentsCollection = client.db("elearning").collection("parents");
+        const studentsCollection = client.db("elearning").collection("students");
+        const teachersCollection = client.db("elearning").collection("teachers");
+        const classesCollection = client.db("elearning").collection("classes");
+        const subjectsCollection = client.db("elearning").collection("subjects");
+        const examsCollection = client.db("elearning").collection("exams");
+        const resultsCollection = client.db("elearning").collection("results");
+        const paymentsCollection = client.db("elearning").collection("payments");
+        const paymentsHistoryCollection = client.db("elearning").collection("paymentsHistory");
+        
+
+        app.get("/parents", async(req, res)=>{
+            const parents = await parentsCollection.find({}).toArray();
+            res.send(parents);
+        })
+    }
+    finally{
+        //await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`);
